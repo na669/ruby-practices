785591638dd52c415414a4fd8e91c191c7ea9bfa
toyama <toyama8717@gmail.com>
2021-09-16 21:35:43 +0900

    Modified-2 ls.rb

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-class Lscommand
-  files = Dir.glob('*')
-  files = files.sort
+files = Dir.glob('*')
+files = files.sort
 
-  column = 3
+COLUMN = 3
 
-  max_character = files.max_by(&:length)
+max_character = files.max_by(&:length)
 
-  original_files_size = files.size
+original_files_size = files.size
 
-  r = files.size % column
-  unless r.zero?
-    (column - r).times do
-      files << nil
-    end
+r = files.size % COLUMN
+unless r.zero?
+  (COLUMN - r).times do
+    files << nil
   end
+end
 
-  row = (original_files_size.to_f / column).ceil
-  files_sliced = files.each_slice(row).to_a
-  files_transed = files_sliced.transpose
+row = (original_files_size.to_f / COLUMN).ceil
+files_sliced = files.each_slice(row).to_a
+files_transed = files_sliced.transpose
 
-  files_transed.each do |f|
-    f.each do |g|
-      printf("%-#{max_character.size + 2}s", g)
-    end
-    print("\n")
+files_transed.each do |f|
+  f.each do |g|
+    printf("%-#{max_character.size + 2}s", g)
   end
+  print("\n")
 end
